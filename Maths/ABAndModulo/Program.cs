1ebe585dc84be9c6fe3aee6aa29bfc9711b19a3f
ManishKshetrapal45 <manishkshetrapal105@gmail.com>
2026-07-08 19:19:40 +0530

    Code for ABAndModulo

---
+++ Maths/ABAndModulo/Program.cs
@@ -0,0 +1,84 @@
+﻿//A, B and Modulo
+/*Problem Description
+
+Given two integers A and B, find the greatest possible positive integer M, such that A % M = B % M.
+
+
+
+Problem Constraints
+
+1 <= A, B <= 109
+A != B
+
+
+
+Input Format
+
+The first argument is an integer A.
+The second argument is an integer B.
+
+
+
+Output Format
+
+Return an integer denoting the greatest possible positive M.
+
+
+
+Example Input
+
+Input 1:
+
+A = 1
+B = 2
+Input 2:
+
+A = 5
+B = 10
+
+
+Example Output
+
+Output 1:
+
+1
+Output 2:
+
+5
+
+
+Example Explanation
+
+Explanation 1:
+
+1 is the largest value of M such that A % M == B % M.
+Explanation 2:
+
+For M = 5, A % M = 0 and B % M = 0.
+
+No value greater than M = 5, satisfies the condition.
+ */
+namespace ABAndModulo
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine(Solve(1, 2));
+      Console.WriteLine(Solve(10, 5));
+    }
+
+    public static int Solve(int A, int B)
+    {
+      int diff = Math.Abs(A - B);
+      if (A % diff == B % diff)
+      {
+        return (diff);
+      }
+      else
+      {
+        return (1);
+      }
+    }
+  }
+}
