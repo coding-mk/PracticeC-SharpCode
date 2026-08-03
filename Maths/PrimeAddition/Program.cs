291226e48b36fc79f046726d26fc84766c6ffdb1
ManishKshetrapal45 <manishkshetrapal105@gmail.com>
2026-08-03 09:50:37 +0530

    Code for 03-08-2026

---
+++ Maths/PrimeAddition/Program.cs
@@ -0,0 +1,45 @@
+//Prime Addition
+/*You are given an even number N and you need to represent the given number as the sum of primes. The prime numbers do not necessarily have to be distinct. It is guaranteed that at least one possible solution exists.
+
+You need to determine the minimum number of prime numbers needed to represent the given number.
+
+Input
+
+The first argument contains an integer N,the number you need to represent (2<=N<=10^9).
+Output
+
+Return an integer which is the minimum number of prime numbers needed to represent the given number N.
+Examples
+
+Input
+
+26
+Output
+
+2
+Explanation
+
+Testcase 1-
+
+You can represent 26 as: 13+13
+So we require minimum of 2 prime numbers to represent the number 26.
+ */
+namespace PrimeAddition
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Solve(26));
+        }
+
+        public static int solve(int A)
+        {
+            if (A == 2)
+            {
+                return (1);
+            }
+            return (2);
+        }
+    }
+}
